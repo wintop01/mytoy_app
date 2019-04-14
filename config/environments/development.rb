26f98031cd7b71c5ca7d628a3293e30cd8576d80
wintop01 <davidcdx@163.com>
2019-04-14 21:40:03 +0800

    disable forgery protection

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Disable request forgery protection in development environment
+  config.action_controller.allow_forgery_protection = false
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -51,4 +54,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
