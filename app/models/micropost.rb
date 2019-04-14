1a1923d520cfeb9cddbc9a3491215d1276bdd3c9
wintop01 <davidcdx@163.com>
2019-04-14 21:51:09 +0800

    add limits

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,4 @@
 class Micropost < ApplicationRecord
+  belong_to :user
   validates :content, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts
   validates :name, length: { maximum: 20 }
+
+
 end
